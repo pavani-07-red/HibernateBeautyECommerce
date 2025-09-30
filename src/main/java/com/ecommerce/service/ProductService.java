466fd236af10f38ecebe92dfd1d4f5086d6ee44d
kolanu <pavanireddykolanu@gmail.com>
2025-09-30 17:45:53 -0400

    Updated Product.java with category field and constructor

--- src/main/java/com/ecommerce/service/ProductService.java
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -14,26 +14,30 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    // Add sample products to DB if none exist
     public void addSampleProducts() {
         if (productRepository.count() == 0) {
-            productRepository.save(new Product("Lipstick", "Kay Beauty", 299.0, "/images/lipstick.jpg"));
-            productRepository.save(new Product("Foundation", "Kay Beauty", 899.0, "/images/foundation.jpg"));
-            productRepository.save(new Product("Eyeliner", "Kay Beauty", 299.0, "/images/eyeliner.jpg"));
-            productRepository.save(new Product("Eyeshadow Palette", "Kay Beauty", 999.0, "/images/eyeshadowpalette.jpg"));
-            productRepository.save(new Product("Lip Crayon", "Kay Beauty", 349.0, "/images/lipcrayon.jpg"));
-            productRepository.save(new Product("Lip Tint", "Kay Beauty", 499.0, "/images/liptint.jpg"));
-            productRepository.save(new Product("Blush", "Kay Beauty", 399.0, "/images/blush.jpg"));
+            productRepository.save(new Product("Lipstick", "Kay Beauty", 299.0, "/images/lipstick.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Foundation", "Kay Beauty", 899.0, "/images/foundation.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Eyeliner", "Kay Beauty", 299.0, "/images/eyeliner.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Eyeshadow Palette", "Kay Beauty", 999.0, "/images/eyeshadowpalette.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Lip Crayon", "Kay Beauty", 349.0, "/images/lipcrayon.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Lip Tint", "Kay Beauty", 499.0, "/images/liptint.jpg", "Kay Beauty"));
+            productRepository.save(new Product("Blush", "Kay Beauty", 399.0, "/images/blush.jpg", "Kay Beauty"));
         }
     }
 
+    // Get all products
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    // Get product by name
     public Product getProductByName(String name) {
         return productRepository.findByName(name);
     }
 
+    // Search product names
     public List<String> searchProductNames(String query) {
         return productRepository.findByNameContainingIgnoreCase(query)
                 .stream()
@@ -41,5 +45,4 @@ public class ProductService {
                 .collect(Collectors.toList());
     }
 }
-
 
